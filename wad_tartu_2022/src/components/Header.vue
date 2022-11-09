14d5a62a2a0f5dc9ed086a5698fb596d7654bb10
Daria Eckert <daria.eckert@student.uni-augsburg.de>
2022-11-09 18:14:33 +0100

    changes to nav links

---
+++ wad_tartu_2022/src/components/Header.vue
@@ -0,0 +1,15 @@
+<template> 
+    <div>
+        <p>Header Component</p>
+        <nav>
+            <router-link to="/Main">Main</router-link> |
+            <router-link to="/Signup">SignUp</router-link> |
+        </nav>
+    </div>
+</template> 
+
+<script>
+export default {
+  name: 'HeaderCompo',
+}
+</script>
