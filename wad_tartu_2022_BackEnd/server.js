c88a356c1809050893ceebd6cb7cf64c999cd8bd
Yarik Möller <yarik.moeller@nordakademie.de>
2022-12-07 12:21:21 +0200

    add authentication functions

--- wad_tartu_2022_BackEnd/server.js
+++ wad_tartu_2022_BackEnd/server.js
@@ -6,11 +6,24 @@ const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
 
+const secret = "gdgdhdbcb770785rgdzqws"; // use a stronger secret
+const maxAge = 60 * 60; //unlike cookies, the expiresIn in jwt token is calculated by seconds not milliseconds
+
+const generateJWT = (id) => {
+    return jwt.sign({ id }, secret, { expiresIn: maxAge })
+        //jwt.sign(payload, secret, [options, callback]), and it returns the JWT as string
+}
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+// We need to include "credentials: true" to allow cookies to be represented  
+// Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
+
+app.use(express.json()); // Parses incoming requests with JSON payloads and is based on body-parser.
+app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
+
+
 
 // Home page: get all posts
 app.get('/posts', async(req, res) => {
@@ -98,37 +111,6 @@ app.delete('/posts/:id', async(req, res) => {
         console.error(err.message);
     }
 });
-
-const express = require('express');
-const pool = require('./database');
-const cors = require('cors');
-const bcrypt = require('bcrypt');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-
-const port = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
-// We need to include "credentials: true" to allow cookies to be represented  
-// Also "credentials: 'include'" need to be added in Fetch API in the Vue.js App
-
-app.use(express.json()); // Parses incoming requests with JSON payloads and is based on body-parser.
-app.use(cookieParser()); // Parse Cookie header and populate req.cookies with an object keyed by the cookie names.
-
-const secret = "gdgdhdbcb770785rgdzqws"; // use a stronger secret
-const maxAge = 60 * 60; //unlike cookies, the expiresIn in jwt token is calculated by seconds not milliseconds
-
-const generateJWT = (id) => {
-    return jwt.sign({ id }, secret, { expiresIn: maxAge })
-        //jwt.sign(payload, secret, [options, callback]), and it returns the JWT as string
-}
-
-app.listen(port, () => {
-    console.log("Server is listening to port " + port)
-});
-
 
 // is used to check whether a user is authinticated
 app.get('/auth/authenticate', async(req, res) => {
@@ -228,6 +210,6 @@ app.get('/auth/logout', (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log("Server is listening to port " + port)
+    console.log("Server is listening to port" + port)
 });
 
